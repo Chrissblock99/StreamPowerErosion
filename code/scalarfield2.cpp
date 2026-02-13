742b308763c346168eea4ef99afb9bb2c3f0fa47
Chriss99 <75170436+Chrissblock99@users.noreply.github.com>
2026-02-13 01:24:05 +0100

    remove stb_image_write

--- code/scalarfield2.cpp
+++ code/scalarfield2.cpp
@@ -4,9 +4,6 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
-
 /*!
 \class ScalarField2 scalarfield.h
 \brief A base two-dimensional field of real values.
